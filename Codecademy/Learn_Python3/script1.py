fe7f8d4cac025683ae2c8ca28ce24d506fbe346f
SahandSoltanieh <47848144+SahandSoltanieh@users.noreply.github.com>
2020-03-25 03:03:11 +0430

    Coded Correspoondence challenge added

--- Codecademy/Learn_Python3/script1.py
+++ Codecademy/Learn_Python3/script1.py
@@ -1,24 +1,31 @@
-highlighted_poems = "Afterimages:Audre Lorde:1997,  The Shadow:William Carlos Williams:1915, Ecstasy:Gabriela Mistral:1925,   Georgia Dusk:Jean Toomer:1923,   Parting Before Daybreak:An Qi:2014, The Untold Want:Walt Whitman:1871, Mr. Grumpledump's Song:Shel Silverstein:2004, Angel Sound Mexico City:Carmen Boullosa:2013, In Love:Kamala Suraiyya:1965, Dream Variations:Langston Hughes:1994, Dreamwood:Adrienne Rich:1987"
-print(highlighted_poems)
-print("\n")
+def decode(message,keyword):
+    alphabet = "abcdefghijklmnopqrstuvwxyz"
+    punctuation = ".,?'! "
+    m_len = len(message)
+    k_len = len(keyword)
+    index = 0
+    crypto = ''
+    for letter in message:
+        if not letter in punctuation:
+            k_index = alphabet.find(keyword[index%k_len])
+            crypto += alphabet[k_index]
+            index += 1
+        else:
+            crypto += letter
+    index = 0
+    translated_message = ''
+    for letter in message:
+        if not letter in punctuation:
+            k_index = alphabet.find(message[index])
+            i_index = alphabet.find(crypto[index])
+            translated_message += alphabet[abs((k_index - i_index)%26)]
+            index += 1
+        else:
+            translated_message += letter
+            index += 1
+    # print(crypto)
+    return translated_message
 
-highlighted_poems_list = highlighted_poems.split(',')
-print(highlighted_poems_list)
-highlighted_poems_stripped = [name.strip() for name in highlighted_poems_list]
-print("\n")
-print(highlighted_poems_stripped)
-print("\n")
-highlighted_poems_details = []
-for name in highlighted_poems_stripped:
-  highlighted_poems_details.append(name.split(':'))
-print(highlighted_poems_details)
-print("\n")
-titles = []
-poets = []
-dates = []
-for name in highlighted_poems_details:
-  titles.append(name[0])
-  poets.append(name[1])
-  dates.append(name[2])
-for i in range(len(highlighted_poems_details)):
-  print("The poem {titles} was published by {poets} in {dates}".format(titles=titles[i],poets=poets[i],dates=dates[i]))
+# print('This is test sentence, Please test this out!')
+# decode('This is test sentence, Please test this out','friends')
+print(decode('dfc aruw fsti gr vjtwhr wznj? vmph otis! cbx swv jipreneo uhllj kpi rahjib eg fjdkwkedhmp!','friends'))
--- Codecademy/Learn_Python3/script2.py
+++ Codecademy/Learn_Python3/script2.py
@@ -1,5 +1,24 @@
-par = 'xuo jxuhu! jxyi yi qd unqcfbu ev q squiqh syfxuh. muhu oek qrbu je tusetu yj? y xefu ie! iudt cu q cuiiqwu rqsa myjx jxu iqcu evviuj!'
-alpha = 'abcdefghijklmnopqrstuvwxyz'
-par_list = par.split()
-print(par_list)
-par_altered = []
+def decode(message,offset):
+    alphabet = "abcdefghijklmnopqrstuvwxyz"
+    punctuation = ".,?'! "
+    translated_message = ""
+    for letter in message:
+        if not letter in punctuation:
+            letter_value = alphabet.find(letter)
+            translated_message += alphabet[(letter_value + offset) % 26]
+        else:
+            translated_message += letter
+    return translated_message
+
+def code(message,offset):
+    translated_message = ""
+    for letter in message_for_v:
+        if not letter in punctuation:
+            letter_value = alphabet.find(letter)
+            translated_message += alphabet[(letter_value - offset) % 26]
+        else:
+            translated_message += letter
+    return translated_message
+
+for i in range(26):
+  print('The encrypted message is : \"'+ decode("vhfinmxkl atox kxgwxkxw tee hy maxlx hew vbiaxkl tl hulhexmx. px'ee atox mh kxteer lmxi ni hnk ztfx by px ptgm mh dxxi hnk fxlltzxl ltyx.",i) + '\". \nOffset:' + str(i) + '\n\n')
