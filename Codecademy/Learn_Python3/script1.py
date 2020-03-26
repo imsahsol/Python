cc89ff8102794af160eb65ccf3cbbc8fa0921dae
SahandSoltanieh <47848144+SahandSoltanieh@users.noreply.github.com>
2020-03-26 23:10:19 +0430

    It was bothering me

--- Codecademy/Learn_Python3/script1.py
+++ Codecademy/Learn_Python3/script1.py
@@ -1,31 +1,18 @@
-def decode(message,keyword):
-    alphabet = "abcdefghijklmnopqrstuvwxyz"
-    punctuation = ".,?'! "
-    m_len = len(message)
-    k_len = len(keyword)
-    index = 0
-    crypto = ''
-    for letter in message:
-        if not letter in punctuation:
-            k_index = alphabet.find(keyword[index%k_len])
-            crypto += alphabet[k_index]
-            index += 1
-        else:
-            crypto += letter
-    index = 0
-    translated_message = ''
-    for letter in message:
-        if not letter in punctuation:
-            k_index = alphabet.find(message[index])
-            i_index = alphabet.find(crypto[index])
-            translated_message += alphabet[abs((k_index - i_index)%26)]
-            index += 1
-        else:
-            translated_message += letter
-            index += 1
-    # print(crypto)
-    return translated_message
+from datetime import datetime
 
-# print('This is test sentence, Please test this out!')
-# decode('This is test sentence, Please test this out','friends')
-print(decode('dfc aruw fsti gr vjtwhr wznj? vmph otis! cbx swv jipreneo uhllj kpi rahjib eg fjdkwkedhmp!','friends'))
+birthday = datetime(1998, 4, 23, 10, 50, 12)
+
+print(birthday.year)
+print(birthday.weekday())
+print(datetime.now())
+print(datetime(2018, 1, 1) - datetime(2017, 1, 1))
+print(datetime.now() - datetime(2018,1,1))
+
+parsed_date = datetime.strptime('Jan 15, 2018', '%b %d, %Y')
+print(parsed_date)
+print(parsed_date.month)
+print(parsed_date.weekday())
+print(parsed_date.year)
+
+date_string = datetime.strftime(datetime.now(), '%b %d, %Y')
+print(date_string)
--- Codecademy/Learn_Python3/script2.py
+++ Codecademy/Learn_Python3/script2.py
@@ -1,24 +1,9 @@
-def decode(message,offset):
-    alphabet = "abcdefghijklmnopqrstuvwxyz"
-    punctuation = ".,?'! "
-    translated_message = ""
-    for letter in message:
-        if not letter in punctuation:
-            letter_value = alphabet.find(letter)
-            translated_message += alphabet[(letter_value + offset) % 26]
-        else:
-            translated_message += letter
-    return translated_message
+location = {}
+location['Paris'] = 100
 
-def code(message,offset):
-    translated_message = ""
-    for letter in message_for_v:
-        if not letter in punctuation:
-            letter_value = alphabet.find(letter)
-            translated_message += alphabet[(letter_value - offset) % 26]
-        else:
-            translated_message += letter
-    return translated_message
+print(location)
 
-for i in range(26):
-  print('The encrypted message is : \"'+ decode("vhfinmxkl atox kxgwxkxw tee hy maxlx hew vbiaxkl tl hulhexmx. px'ee atox mh kxteer lmxi ni hnk ztfx by px ptgm mh dxxi hnk fxlltzxl ltyx.",i) + '\". \nOffset:' + str(i) + '\n\n')
+place = dict()
+place.update({'Paris':100})
+
+print(place)
